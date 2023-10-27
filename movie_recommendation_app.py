7553c012e8bc9c9a3a8b772185a4a4da2e20bcfc
User001 <12345@12345.com>
2023-10-27 17:09:07 +0900

    movie_recommendation, pyqt : 20231027, pyqt and recommendation1.

--- movie_recommendation_app.py
+++ movie_recommendation_app.py
@@ -75,18 +75,18 @@ class Exam(QWidget, form_window):
         except:
             return '다른 키워드를 이용하세요'
 
-    def combobox_slot(self):
+    def combobox_slot(self):        # 슬롯 정의
         title = self.comboBox.currentText()
         recommendation = self.recommendation_by_movie_title(title)
         self.lbl_recommendation.setText(recommendation)
 
-    def recommendation_by_movie_title(self, title):
+    def recommendation_by_movie_title(self, title):     # 영화 추천
         movie_idx = self.df_reviews[self.df_reviews['titles']==title].index[0]
         cosine_sim = linear_kernel(self.Tfidf_matrix[movie_idx], self.Tfidf_matrix)
         recommendation = self.getRecommendation(cosine_sim)
         return recommendation
 
-    def getRecommendation(self, cosine_sim):
+    def getRecommendation(self, cosine_sim):        # 추천 영화 리스트
         simScore = list(enumerate(cosine_sim[-1]))
         simScore = sorted(simScore, key=lambda x: x[1], reverse=True)
         simScore = simScore[:11]
